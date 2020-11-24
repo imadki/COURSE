12a710359ab7288c1aac88a3f4943ae5aa370939
kissami <imadki>
2020-11-24 15:19:26 +0100

    C course

---
+++ C/LSD-2A/code/limits.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main(){
+
+  printf("%d\n", INT_MIN);
+  printf("%d\n", INT_MAX);
+
+  unsigned int var = 4294967296;
+  
+  printf("%u\n", var);
+
+  return 0;
+
+}
